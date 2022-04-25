52e5db01297637616c7e2fe80af90b3b957d2880
Zaida Hammel <99354763+zaida-hammel@users.noreply.github.com>
2022-04-25 16:58:58 +0000

    edited code

--- js/script.js
+++ js/script.js
@@ -1,8 +1,43 @@
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Copyright (c) 2022 Zaida Hammel All rights reserved
+//
+// Created by: Zaida Hammel
+// Created on: Apr 2022
 // This file contains the JS functions for index.html
 
+"use strict"
+
+/**
+ * Check servie worker.
+ */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-Unit5-01-HTML/sw.js", {
+    scope: "/ICS2O-Unit5-01-HTML/",
+  })
+}
+
+const randomNumber = Math.floor(Math.random() * 6) + 1
+
+/**
+ * This function updates the slider value.
+ */
+function updateSliderValue(valueFromSlider) {
+  document.getElementById("slider-value").innerHTML = valueFromSlider
+}
+
+/**
+ * This function compares slider with random number.
+ */
 function myButtonClicked() {
-  // this function displays "Hello, World!"
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>";
+  const sliderValue = slider.value
+  
+  // process
+  if (sliderValue == randomNumber) {
+    document.getElementById('answer').innerHTML = 'The answer was, ' + randomNumber +'!' +  ' You got it! Good job.'
+   // console.log("Congratulations!")
+  }
+  //  block of code to be executed if condition1 is true
+  if (sliderValue != randomNumber) {
+    document.getElementById('answer').innerHTML = 'The answer was, ' + randomNumber +'!' +  ' Nice guess, but try again.'
+   // console.log("Try again!")
+  }
 }
